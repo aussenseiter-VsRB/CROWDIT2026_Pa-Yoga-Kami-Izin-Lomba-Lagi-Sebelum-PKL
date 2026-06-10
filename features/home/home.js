a7e756d8cd09a234dfde68b27dbed5374644cea9
max-tantan <fatanjamterbang@gmail.com>
2026-06-10 17:17:26 +0700

    feat: implement floating action button for creating forums and update related styles

--- features/home/home.js
+++ features/home/home.js
@@ -2,7 +2,7 @@ import { injectStyle } from '../../js/utils/styleLoader.js';
 import { fetchData } from '../../js/utils/api.js';
 import { asset, navigateTo } from '../../js/utils/url.js';
 import { DATA_PATHS, MOBILE_BREAKPOINT } from '../../js/core/config.js';
-import { getSession } from '../../js/services/auth.js';
+import { getSession, isAuthenticated } from '../../js/services/auth.js';
 import { mergeCourseData } from './js/_utils.js';
 import { renderDesktop, renderMobile } from './js/_render.js';
 import { bindTopicTabs } from './js/_handlers.js';
